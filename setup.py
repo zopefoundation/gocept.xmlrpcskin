b48ac834406f211c0191a5e4ab9756103781c17d
wosc <wosc@wosc.de>
2011-09-22 12:12:55 +0000

    Back to development: 1.1
    git-svn-id: file:///Users/mac/vcs/main/gocept.xmlrpcskin/trunk@122891 62d5b8a3-27da-0310-9561-8e5933582275

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='gocept.xmlrpcskin',
-    version='1.0',
+    version='1.1dev',
     author='gocept',
     author_email='mail@gocept.com',
     url='http://pypi.python.org/pypi/gocept.xmlrpcskin',
